b18e0717a4f435fd1c2404894b604e31151b52d6
Tamyris Torres <wedellatorres@gmail.com>
2022-01-02 15:23:49 -0300

    programa de cadastro de alunos

---
+++ DigitalInnovationOne/EstruturaDoPrograma/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        
+        static void Main()
+        {
+            var s = new Pilha();
+            s.Empilhar(1);
+            s.Empilhar(10);
+            s.Empilhar(100);
+            Console.WriteLine(s.Desempilhar());
+            Console.WriteLine(s.Desempilhar());
+            Console.WriteLine(s.Desempilhar());
+        }
+    }
+}
